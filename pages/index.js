0e9a263439d75549d26e2d1dae9295a547365e0b
v-octal <vikas.rajput54@gmail.com>
2021-12-07 23:30:10 +0530

    feat: added main pages

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,95 @@
-import Link from 'next/link'
+import React from "react";
+import Link from "next/link";
 
-export default function Home() {
+// components
+
+import Navbar from "components/Navbars/AuthNavbar.js";
+import Footer from "components/Footers/Footer.js";
+
+export default function Landing() {
   return (
-    <ul>
-      <li>
-        <Link href="/b" as="/a">
-          <a>a</a>
-        </Link>
-      </li>
-      <li>
-        <Link href="/a" as="/b">
-          <a>b</a>
-        </Link>
-      </li>
-    </ul>
-  )
+    <>
+      <Navbar transparent />
+      <main>
+        <section className="relative py-20">
+          <div
+            className="bottom-auto top-0 left-0 right-0 w-full absolute pointer-events-none overflow-hidden -mt-20 h-20"
+            style={{ transform: "translateZ(0)" }}
+          >
+            <svg
+              className="absolute bottom-0 overflow-hidden"
+              xmlns="http://www.w3.org/2000/svg"
+              preserveAspectRatio="none"
+              version="1.1"
+              viewBox="0 0 2560 100"
+              x="0"
+              y="0"
+            >
+              <polygon
+                className="text-white fill-current"
+                points="2560 0 2560 100 0 100"
+              ></polygon>
+            </svg>
+          </div>
+
+          <div className="container mx-auto px-4">
+            <div className="items-center flex flex-wrap">
+              <div className="w-full md:w-5/12 ml-auto mr-auto px-4">
+                <img
+                  alt="..."
+                  className="max-w-full rounded-lg shadow-lg"
+                  src="https://images.unsplash.com/photo-1560174038-da43ac74f01b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2914&q=80"
+                />
+              </div>
+              <div className="w-full md:w-5/12 ml-auto mr-auto px-4">
+                <div className="md:pr-12">
+                  <h3 className="text-6xl font-semibold">Fight Club</h3>
+                  <ol className="list-decimal mt-6 px-6">
+                    <li className="py-2">
+                      <div className="flex items-center">
+                        <div>
+                          <h4 className="text-blueGray-500 text-lg">
+                            You do not talk about Fight Club.
+                          </h4>
+                        </div>
+                      </div>
+                    </li>
+                    <li className="py-2">
+                      <div className="flex items-center">
+                        <div>
+                          <h4 className="text-blueGray-500 text-lg">
+                            You do not talk about Fight Club.
+                          </h4>
+                        </div>
+                      </div>
+                    </li>
+                    <li className="py-2">
+                      <div className="flex items-center">
+                        <div>
+                          <h4 className="text-blueGray-500 text-lg">
+                            Winner takes all.
+                          </h4>
+                        </div>
+                      </div>
+                    </li>
+                    <li className="py-2">
+                      <div className="flex items-center">
+                        <div>
+                          <h4 className="text-blueGray-500 text-lg">
+                            If this is your first time at Fight Club, you have
+                            to fight.
+                          </h4>
+                        </div>
+                      </div>
+                    </li>
+                  </ol>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </main>
+      <Footer />
+    </>
+  );
 }
